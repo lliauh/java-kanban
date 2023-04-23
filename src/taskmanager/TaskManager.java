1303f1548423e9285e19856042f26a5181430956
lliauh <lliauh@gmail.com>
2023-04-23 22:14:26 +0300

    fix: исправить модификаторы доступа в интерфейсе TaskManager

--- src/taskmanager/TaskManager.java
+++ src/taskmanager/TaskManager.java
@@ -5,41 +5,41 @@ import java.util.ArrayList;
 
 public interface TaskManager {
 
-    public int createTask(Task task);
+    int createTask(Task task);
 
-    public int createEpic(Epic epic);
+    int createEpic(Epic epic);
 
-    public int createSubtask(Subtask subtask, Integer epicId);
+    int createSubtask(Subtask subtask, Integer epicId);
 
-    public void updateTask(Task task);
+    void updateTask(Task task);
 
-    public void updateEpic(Epic epic);
+    void updateEpic(Epic epic);
 
-    public void updateSubtask(Subtask subtask);
+    void updateSubtask(Subtask subtask);
 
-    public List getAllTasks();
+    List getAllTasks();
 
-    public List getAllEpics();
+    List getAllEpics();
 
-    public List getAllSubtasks();
+    List getAllSubtasks();
 
-    public void removeAllTasks();
+    void removeAllTasks();
 
-    public void removeAllEpics();
+    void removeAllEpics();
 
-    public void removeAllSubtasks();
+    void removeAllSubtasks();
 
-    public Task getTaskById(Integer taskId);
+    Task getTaskById(Integer taskId);
 
-    public Epic getEpicById(Integer epicId);
+    Epic getEpicById(Integer epicId);
 
-    public Subtask getSubtaskById(Integer subtaskId);
+    Subtask getSubtaskById(Integer subtaskId);
 
-    public void removeTaskById(Integer taskId);
+    void removeTaskById(Integer taskId);
 
-    public void removeSubtaskById(Integer subtaskId);
+    void removeSubtaskById(Integer subtaskId);
 
-    public void removeEpicById(Integer epicId);
+    void removeEpicById(Integer epicId);
 
-    public ArrayList<Subtask> getSubTasksByEpic(Epic epic);
+    ArrayList<Subtask> getSubTasksByEpic(Epic epic);
 }
